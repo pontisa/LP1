fc0f2b25a9d0a214bf7cd404bebc7afc358e81b0
Isabella Pontes <pontisa@protonmail.com>
2023-10-10 12:17:28 -0300

    commit inicial

---
+++ 0030482321022/Lista1ex1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+//Isabella Pontes 0030482321022
+
+int main(){
+	long long int RA;
+	
+	scanf("%lld",&RA);
+	printf("%lld Sucesso",RA);
+	
+	/*typedef struct {
+		int escola[3];
+		int curso[3];
+		int ano[2];
+		int semestre[1];
+		int turno[1];
+		int sequencial[3];		
+	}; partesRA;*/
+
+	return 0;
+}
